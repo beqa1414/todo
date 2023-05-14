25a3a28283f2c791ae7aba52cff2e576d8a401cc
beka shamugia <68068249+beqa1414@users.noreply.github.com>
2023-05-14 16:32:07 +0400

    add todo

--- src/App.js
+++ src/App.js
@@ -1,34 +1,11 @@
 import { useState } from "react";
 import "./App.css";
-import rect from "./images/Rectangle 2.png";
-import List from "./Components/List";
+import Todo from "./Components/Todo";
 
 function App() {
-  const [itemName, setItemName] = useState(null);
-  const [itemList, updateItemList] = useState([]);
-
-  const onChangeHandler = (e) => {
-    setItemName(e.target.value);
-  };
-  const addItemsList = () => {
-    updateItemList([...itemList, { item: itemName, key: Date.now() }]);
-    setItemName("");
-  };
-  console.log(onChangeHandler);
   return (
     <div className="App">
-      <div className="todo-header">
-        <img src={rect} alt="" className="img_rect" />
-        <div className="thur">
-          <p className="thru_text">Thur 9</p>
-          <h1 className="am">6:23 AM</h1>
-        </div>
-      </div>
-      <div className="input-wrapper">
-        <input type="text" value={itemName} onChange={onChangeHandler} />
-        <button onClick={addItemsList}>+</button>
-      </div>
-      <List itemList={itemList} updateItemList={updateItemList} />
+      <Todo />
     </div>
   );
 }
---
+++ src/Components/Todo.js
@@ -0,0 +1,41 @@
+import { useState } from "react";
+import rect from "../images/Rectangle 2.png";
+import List from "./List";
+
+function Todo() {
+  const [itemName, setItemName] = useState(null);
+  const [itemList, updateItemList] = useState([]);
+
+  const onChangeHandler = (e) => {
+    setItemName(e.target.value);
+  };
+  const addItemsList = () => {
+    updateItemList([...itemList, { item: itemName, key: Date.now() }]);
+    setItemName("");
+  };
+  console.log(onChangeHandler);
+  return (
+    <div className="App">
+      <div className="todo-header">
+        <img src={rect} alt="" className="img_rect" />
+        <div className="thur">
+          <p className="thru_text">Thur 9</p>
+          <h1 className="am">6:23 AM</h1>
+        </div>
+      </div>
+      <div className="input-wrapper">
+        <input
+          type="text"
+          className="input_frame"
+          placeholder="Note"
+          value={itemName}
+          onChange={onChangeHandler}
+        />
+        <button onClick={addItemsList}>+</button>
+      </div>
+      <List itemList={itemList} updateItemList={updateItemList} />
+    </div>
+  );
+}
+
+export default Todo;
